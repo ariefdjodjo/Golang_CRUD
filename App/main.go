02c7d858ae66a5cc1c6ba7f1f2b4b9c0e67f401a
ariefdjodjo <arief_djodjo@yahoo.com>
2019-07-10 13:18:35 +0700

    membuat handle route

--- App/main.go
+++ App/main.go
@@ -21,7 +21,7 @@ type Mahasiswa struct {
 }
 
 type Jurusan struct {
-	IdJurusan 	int 	
+	Id_Jurusan 	int 	
 	NamaJurusan	string	
 }
 
@@ -32,7 +32,7 @@ func dbConn() (db *sql.DB)  {
 
 	dbUser := "root"
 	dbName := "kampus"
-	db, err := sql.Open("postgres", "postgresql://"+dbUser+"@"+os.Getenv("ROACH_HOST")+":26257/"+dbName+"?sslmode=enable")
+	db, err := sql.Open("postgres", "postgresql://"+dbUser+"@"+os.Getenv("ROACH_HOST")+":26257/"+dbName+"?sslmode=disable")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -68,11 +68,57 @@ func dataJurusan(w http.ResponseWriter, r *http.Request) {
 	err = tmpl.ExecuteTemplate(w, "dataJurusan", data)
 }
 
+func tambahJurusan(w http.ResponseWriter, r *http.Request) {
+	var data = map[string]interface{}{
+		"title": "golang CRUD",
+	}
+
+	err = tmpl.ExecuteTemplate(w, "tambahJurusan", data)
+}
+
+func tambah(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		id_jurusan := r.FormValue("id_jurusan")
+		nama_jurusan := r.FormValue("nama_jurusan")
+
+		insForm, err := db.Prepare("INSERT INTO jurusan (id_jurusan, nama_jurusan) VALUES ($1,$2)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(id_jurusan, nama_jurusan)
+		log.Println("INSERT: nama: " + id_jurusan + " | alamat: " + nama_jurusan)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func dataMahasiswa(w http.ResponseWriter, r *http.Request) {
+	err = tmpl.ExecuteTemplate(w, "dataMahasiswa", nil)
+}
+
+func tambahMahasiswa(w http.ResponseWriter, r *http.Request) {
+	err = tmpl.ExecuteTemplate(w, "tambahMahasiswa", nil)
+}
+
+func tambahM(w http.ResponseWriter, r *http.Request) {
+	err = tmpl.ExecuteTemplate(w, "tambahM", nil)
+}
+
+
 //membuat main
 func main() {
 	log.Println("Server started on: http://localhost:8010")
 	http.HandleFunc("/", index)
 	http.HandleFunc("/dataJurusan", dataJurusan)
+	http.HandleFunc("/tambahJurusan", tambahJurusan)
+	http.HandleFunc("/tambah", tambah)
+
+	//handle untuk mahasiswa
+	http.HandleFunc("/dataMahasiswa", dataMahasiswa)
+	http.HandleFunc("/tambahMahasiswa", tambahMahasiswa)
+	http.HandleFunc("/tambahM", tambahM)
+
 
 	http.ListenAndServe(":8010", nil)
 }
